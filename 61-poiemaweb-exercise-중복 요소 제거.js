182da9745753c0af6156e7b9dfbe7a6a7b44ec8a
bg-shorthand <hanhanhan333@gmail.com>
2020-12-27 15:51:23 +0900

    중복 요소 제거, 중복 없는 배열

---
+++ 61-poiemaweb-exercise-중복 요소 제거.js
@@ -0,0 +1,9 @@
+// 인수로 전달된 배열의 요소 중에서 중복된 요소를 제외하고 유니크한 요소만을 반환하는 함수를 작성하라.
+
+// for 문은 사용하지 않도록 하자.
+
+function uniq (array) {
+  return array.filter((v, i, arr) => arr.indexOf(v) === i);
+}
+
+console.log(uniq([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
