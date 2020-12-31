3b87425ee98b74896d3b12779735d39fb30423f5
bg-shorthand <hanhanhan333@gmail.com>
2020-12-31 22:20:39 +0900

    소수 찾기, 피보나치 수

---
+++ 66-poiemaweb-exercise-피보나치 수.js
@@ -0,0 +1,20 @@
+// 피보나치 수는 0과 1로 시작하며, 다음 피보나치 수는 바로 앞의 두 피보나치 수의 합이 된다. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946…
+
+// 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 반환하는 fibonacci 함수를 작성하라. 예를 들어 n = 3이라면 2를 반환한다.
+
+function fibonacci(n) {
+  arr = [0, 1];
+
+  for (let i = 0; i < n; i++) {
+    arr.push(arr[i] + arr[i + 1]);
+    console.log(arr);
+  }
+  
+  return arr[arr.length - 2];
+}
+
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
+console.log(fibonacci(6)); // 8
