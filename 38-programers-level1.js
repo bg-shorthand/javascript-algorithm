71d92fee5a91e68ea33db4b0d91c4550b95d050d
bg-shorthand <hanhanhan333@gmail.com>
2021-02-23 00:02:30 +0900

    final answer

--- 38-programers-level1.js
+++ 38-programers-level1.js
@@ -1,26 +1,12 @@
-function solution(n) {
+function solution (n) {
   let answer = 0;
-  let array = Array.from({ length: n }, (_, i) => i + 1);
+  const array = Array.from({ length: n }, (_, i) => i + 1);
 
-  function judge (num) {
-    let res = true;
-    const sqrt = Math.sqrt(num);
-    let count = 0;
-    for (let i = 1; i <= sqrt; i++) {
-      if (num % i === 0) count++;
-      if (count > 1 || num === 1) {
-        res = false;
-        break;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] !== 0) {
+      for (let j = i + array[i]; j <= n; j += array[i]) {
+        array[j] = 0;
       };
-    }
-    return res;
-  }
-
-  while (array.length > 1) {
-    let num = array[1];
-
-    if (judge(num)) {
-      array = array.filter(v => v % num !== 0);
       answer++;
     }
   }
@@ -28,4 +14,4 @@ function solution(n) {
   return answer;
 }
 
-console.log(solution(10));
+console.log(solution(100));
